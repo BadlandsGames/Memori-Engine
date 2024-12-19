773603eba29eb05ca86b51f6144506d14417babb
Hackbot22 <80085651+Hackbot22@users.noreply.github.com>
2024-12-18 18:14:06 -0700

    made changes

--- editor/index.php
+++ editor/index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title></title>
+        <?php
+            $current_project = "Start Page";
+        ?>
+        <title><?php echo "Memori Engine - " . $current_project; ?></title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
