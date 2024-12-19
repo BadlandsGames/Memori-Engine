26b3d6c02427497396666da656602b65f34b4c59
Hackbot22 <80085651+Hackbot22@users.noreply.github.com>
2024-12-18 18:26:57 -0700

    made changes

--- editor/index.php
+++ editor/index.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <?php
-            $current_project = "Start Page";
-            $current_project_fs = array();
-            function open_project($filename) {
-                if(end(str_split($filename)) == "json") {
-                    $newfile_open = fopen($filename, "r") or die("Unable to open file.");
-                    $project_info = json_decode(fread($newfile_open, filesize($newfile_open)), true);
-                    fclose($newfile_open);
-                    $current_project = $project_info["name"];
-                    $current_project_fs = $project_info["filesystem"];
-                } else {
-                    die("File must be a JSON file.");
+        <script type="text/javascript">
+            <?php
+                $current_project = "Start Page";
+                $current_project_fs = array();
+                function open_project($filename) {
+                    if(end(str_split($filename)) == "json") {
+                        $newfile_open = fopen($filename, "r") or die("Unable to open file.");
+                        $project_info = json_decode(fread($newfile_open, filesize($newfile_open)), true);
+                        fclose($newfile_open);
+                        $current_project = $project_info["name"];
+                        $current_project_fs = $project_info["filesystem"];
+                    } else {
+                        die("File must be a JSON file.");
+                    }
                 }
-            }
-        ?>
+            ?>
+        </script>
         <title><?php echo "Memori Engine - " . $current_project; ?></title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
