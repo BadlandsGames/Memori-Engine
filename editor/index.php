60d9aac33cc80935fda312935d8ce64682bf6767
Hackbot22 <80085651+Hackbot22@users.noreply.github.com>
2024-12-18 18:23:43 -0700

    made changes

--- editor/index.php
+++ editor/index.php
@@ -3,6 +3,18 @@
     <head>
         <?php
             $current_project = "Start Page";
+            $current_project_fs = array();
+            function open_project($filename) {
+                if(end(str_split($filename)) == "json") {
+                    $newfile_open = fopen($filename, "r") or die("Unable to open file.");
+                    $project_info = json_decode(fread($newfile_open, filesize($newfile_open)), true);
+                    fclose($newfile_open);
+                    $current_project = $project_info["name"];
+                    $current_project_fs = $project_info["filesystem"];
+                } else {
+                    die("File must be a JSON file.");
+                }
+            }
         ?>
         <title><?php echo "Memori Engine - " . $current_project; ?></title>
         <meta charset="utf-8">
